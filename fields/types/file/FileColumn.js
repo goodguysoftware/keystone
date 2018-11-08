282a37f71f297412a1b9325e092362ff8a0eb8f2
Duong Tran <tranduongms@gmail.com>
2018-11-08 12:07:56 +0700

    Image preview for File type

--- fields/types/file/FileColumn.js
+++ fields/types/file/FileColumn.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ItemsTableCell from '../../components/ItemsTableCell';
 import ItemsTableValue from '../../components/ItemsTableValue';
+import ImageThumbnail from '../../components/ImageThumbnail';
 
 var LocalFileColumn = React.createClass({
 	renderValue: function () {
@@ -13,11 +14,23 @@ var LocalFileColumn = React.createClass({
 		var value = this.props.data.fields[this.props.col.path];
 		var href = value && value.url ? value.url : null;
 		var label = value && value.filename ? value.filename : null;
-		return (
-			<ItemsTableCell href={href} padded interior field={this.props.col.type}>
-				<ItemsTableValue>{label}</ItemsTableValue>
-			</ItemsTableCell>
-		);
+		if (value && value.mimetype && value.mimetype.indexOf('image/') >= 0)
+			return (
+				<ImageThumbnail
+					component="a"
+					href={href}
+					target="__blank"
+					style={{ marginRight: '1em', maxWidth: '50%' }}
+				>
+					<img src={href} style={{ 'max-height': 60, 'max-width': '100%' }} />
+				</ImageThumbnail>
+			)
+		else
+			return (
+				<ItemsTableCell href={href} padded interior field={this.props.col.type}>
+					<ItemsTableValue>{label}</ItemsTableValue>
+				</ItemsTableCell>
+			);
 	},
 });
 
